6b5a1e33d63c154bb6cee68caf4261212868d2df
Durant <gunnar.durant@protonmail.com>
2026-06-29 22:28:41 -0700

    milestones 1.1, 1.2, 1.3

--- src/inventory/ProductTest.java
+++ src/inventory/ProductTest.java
@@ -7,8 +7,10 @@ public class ProductTest {
         Product item2 = new Product();
 
         //four items using values
-        Product item3 = new Product("Charizard", 12, 85.50);
-        Product item4 = new Product("Pikachu")
+        Product item3 = new Product("Charizard", "TrainerBox", 12, 85.50);
+        Product item4 = new Product("Pikachu", "Plushy", 25, 38.75);
+        Product item5 = new Product("Squirtle", "tradingCard", 4, 25.15);
+        Product item6 = new Product("Master Ball", "toy", 3, 45.50);
 
     }
 }
