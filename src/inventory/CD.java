1cce05e587112e79e9263d5b01303ba78d4e3534
Durant <gunnar.durant@protonmail.com>
2026-07-27 07:12:51 -0700

    milestone 5

--- src/inventory/CD.java
+++ src/inventory/CD.java
@@ -1,13 +1,13 @@
 package inventory;
 
-// JP 5 2-a create subclass of product class called DVD
+// JP 5 2-d create subclass of product class called DVD
 public class CD extends Product {
-    // 2-a additional instances
+    // 2-d additional instances
     private int artist;
     private int numberOfSongs;
     private String label;
 
-    // 2-b constructor using super
+    // 2-d constructor using super
     public CD (int itemNumber, String name, int QtyInStock, double price,
                 int artist, int numberOfSongs, String label){
         super(itemNumber, name, QtyInStock, price);
@@ -16,12 +16,12 @@ public class CD extends Product {
         this.label = label;
     }
 
-    // 2-c create getters
+    // 2-d create getters
     public int getArtist(){ return artist; }
     public int getNumberOfSongs(){ return numberOfSongs; }
     public String getLabel(){ return label; }
 
-    // 2-c create setters
+    // 2-d create setters
     public void setLengthMinutes(){}
     public void setAgeRating(){}
     public void setFilmStudio(){}
